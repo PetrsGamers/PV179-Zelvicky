7ea6d75c5e54fc1b743542a277d75618395ff344
Petr Hybl <phybl@redhat.com>
2024-10-22 23:39:58 +0200

    added CRUD to User and search for CountryByName

--- DAL/BL/DTOs/UserDto.cs
+++ DAL/BL/DTOs/UserDto.cs
@@ -6,6 +6,8 @@ public class UserDto
 
     public string Username { get; set; }
 
+    public string Password { get; set; }
+
     public string Email { get; set; }
 
     public List<AlbumDto> Albums { get; set; }
