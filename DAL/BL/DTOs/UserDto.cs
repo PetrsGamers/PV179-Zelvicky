bc4182e8a19f3b67e4a7108c4cebba8b15b90901
Petr Hybl <phybl@redhat.com>
2024-10-22 21:51:23 +0200

    added UserDTO

---
+++ DAL/BL/DTOs/UserDto.cs
@@ -0,0 +1,14 @@
+namespace DAL.BL.DTOs;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+
+    public string Username { get; set; }
+
+    public string Email { get; set; }
+
+    public List<AlbumDto> Albums { get; set; }
+
+    public string Role { get; set; }
+}
