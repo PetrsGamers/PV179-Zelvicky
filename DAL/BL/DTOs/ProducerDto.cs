3010109d3e556390dfcc1890288b4e5061593d36
David Novák <xnovak2x@vutbr.cz>
2024-10-23 23:12:21 +0200

    feat: POST and PUT for producer

--- DAL/BL/DTOs/ProducerDto.cs
+++ DAL/BL/DTOs/ProducerDto.cs
@@ -1,10 +1,6 @@
 namespace DAL.BL.DTOs;
 
-public class ProducerDto{
+public class ProducerDto : ProducerInsertDto
+{
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string City { get; set; }
-    public string Description { get; set; }
-    public Guid Country { get; set; }
-    public Guid? IsEditFor { get; set; }
 }
