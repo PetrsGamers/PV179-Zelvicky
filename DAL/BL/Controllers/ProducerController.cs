9c6256b37714348c5fa492bb7b0a747476a3b977
David Novák <xnovak2x@vutbr.cz>
2024-10-23 23:12:21 +0200

    feat: POST and PUT for producer

--- DAL/BL/Controllers/ProducerController.cs
+++ DAL/BL/Controllers/ProducerController.cs
@@ -1,9 +1,9 @@
+namespace DAL.BL.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace DAL.BL.Controllers;
-
 using DTOs;
+using Entities;
 using Microsoft.Extensions.Logging;
 
 [ApiController]
@@ -82,6 +82,62 @@ public class ProducerController(ApplicationContext context, ILogger<ProducerCont
             context.Producers.Remove(producer);
             await context.SaveChangesAsync();
             return this.NoContent();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            return this.StatusCode(500, "Internal server error.");
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProducer([FromBody] ProducerInsertDto producerDto)
+    {
+        try
+        {
+            var producer = new Producer
+            {
+                Id = Guid.NewGuid(),
+                Name = producerDto.Name,
+                City = producerDto.City,
+                Description = producerDto.Description,
+                CountryId = producerDto.Country,
+                IsEditForId = producerDto.IsEditFor
+            };
+
+            await context.Producers.AddAsync(producer);
+            await context.SaveChangesAsync();
+
+            return this.Ok(producer);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            return this.StatusCode(500, "Internal server error.");
+        }
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateProducer(Guid id, [FromBody] ProducerInsertDto producerDto)
+    {
+        try
+        {
+            var producer = await context.Producers.FindAsync(id);
+            if (producer == null)
+            {
+                return this.NotFound($"Producer with ID {id} not found.");
+            }
+
+            producer.Name = producerDto.Name;
+            producer.City = producerDto.City;
+            producer.Description = producerDto.Description;
+            producer.CountryId = producerDto.Country;
+            producer.IsEditForId = producerDto.IsEditFor;
+
+            context.Producers.Update(producer);
+            await context.SaveChangesAsync();
+
+            return this.Ok(producer);
         }
         catch (Exception ex)
         {
