772f56359b920f0d94c65262c24d02f9dbfa4abc
Tadeas Kozub <tedkozub@github.com>
2024-12-08 21:48:51 +0100

    feat: add postgres validator with ext methods

---
+++ CapEnjoyer.API/Helpers/PostgresOptionValidator.cs
@@ -0,0 +1,45 @@
+﻿namespace CapEnjoyer.API.Helpers;
+
+using DAL;
+
+public class PostgresOptionValidator
+{
+    public required string? Host { get; set; }
+    public required string? Port { get; set; }
+    public required string? Username { get; set; }
+    public required string? Password { get; set; }
+    public required string? Database { get; set; }
+
+    public string ConnectionString =>
+        $"Host={Host};Port={Port};Username={Username};Password={Password};Database={Database}";
+
+    public bool IsValid(WebApplication webApplication)
+    {
+        using var scope = webApplication.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<CapEnjoyerDbContext>();
+
+        if (dbContext.Database.CanConnect())
+        {
+            Console.WriteLine("Database connection validated successfully.");
+            return true;
+        }
+
+        Console.WriteLine("Database is not reachable. Exiting...");
+        return false;
+    }
+}
+
+public static class PostgresOptionValidatorExtensions
+{
+    public static void ValidateConnection(this WebApplication webApplication, PostgresOptionValidator validator)
+    {
+        if (!validator.IsValid(webApplication))
+        {
+            throw new HttpRequestException("Database validation failed.");
+        }
+    }
+}
+
+// Then in program.cs all you need to do is:
+// var mssqlValidator = new MssqlOptionValidator { ... set the data here ... }
+// app.ValidateConnection(mssqlValidator);
--- CapEnjoyer.API/Program.cs
+++ CapEnjoyer.API/Program.cs
@@ -11,13 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
 
-Helpers.CheckIfEnvironmentVariablesAreSet();
-var connectionString = Helpers.GetConnectionString();
+var postgreValidator = new PostgresOptionValidator
+{
+    Host = Environment.GetEnvironmentVariable("DB_HOST"),
+    Port = Environment.GetEnvironmentVariable("DB_PORT"),
+    Username = Environment.GetEnvironmentVariable("DB_USERNAME"),
+    Password = Environment.GetEnvironmentVariable("DB_PASSWORD"),
+    Database = Environment.GetEnvironmentVariable("DB_NAME")
+};
 
 builder.Services.AddControllers(options => options.RespectBrowserAcceptHeader = true).AddXmlSerializerFormatters();
 
 builder.Services.AddDbContextFactory<CapEnjoyerDbContext>(
-    options => options.UseNpgsql(connectionString));
+    options => options.UseNpgsql(postgreValidator.ConnectionString));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -57,7 +63,7 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // in case the database is not reachable or not created, throw an error
-Helpers.CheckIfDatabaseServerReachableAndCreated(app);
+app.ValidateConnection(postgreValidator);
 
 app.UseSwagger();
 app.UseSwaggerUI();
